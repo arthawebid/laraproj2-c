38116fab2bb6932eb8cde41948dc0f638b4dac07
MhSTIKI <arthawebid@gmail.com>
2025-12-02 10:46:12 +0800

    update 1

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\temansController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/temans',temansController::class);
